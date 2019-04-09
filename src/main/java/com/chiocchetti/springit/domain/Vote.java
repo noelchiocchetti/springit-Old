931c6bc34efc9be332b066e449e16c727c33a3e9
Noel Chiocchetti <nc@chiocchetti.ch>
2019-04-09 11:45:53 +0200

    Created our entities and refactored using Project Lombok

---
+++ src/main/java/com/chiocchetti/springit/domain/Vote.java
@@ -0,0 +1,24 @@
+package com.chiocchetti.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    // private User user;
+    // private Link link;
+    private int vote;
+
+    // User
+    // Link
+}

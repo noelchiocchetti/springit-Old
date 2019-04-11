94e237dcce1643b90f821324a95c389031d02359
Noel Chiocchetti <nc@chiocchetti.ch>
2019-04-11 15:43:13 +0200

    Adding the View layer with a lot of stuff

--- src/main/java/com/chiocchetti/springit/SpringitApplication.java
+++ src/main/java/com/chiocchetti/springit/SpringitApplication.java
@@ -1,54 +1,28 @@
 package com.chiocchetti.springit;
 
-import com.chiocchetti.springit.config.SpringitProperties;
-import com.chiocchetti.springit.domain.Comment;
-import com.chiocchetti.springit.domain.Link;
-import com.chiocchetti.springit.repository.CommentRepository;
-import com.chiocchetti.springit.repository.LinkRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import java.util.Arrays;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SpringitProperties.class)
+@EnableJpaAuditing
 public class SpringitApplication {
 
-	@Autowired
-	private SpringitProperties springitProperties;
-	private ApplicationContext applicationContext;
+	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-	//@Bean
-	@Profile("dev")
-	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-		return args -> {
-			System.out.println("Welcome Message: " + springitProperties.getWelcomeMsg());
-			System.out.println("Printing out all the bean names in the application context:");
-			System.out.println("-----------------------------------------------------------");
-
-			Link link = new Link("Getting Started with Spring Boot 2","https://therealdanvega.com/spring-boot-2");
-			linkRepository.save(link);
-
-			Comment comment = new Comment("This Spring Boot 2 Link is awesome",link);
-			commentRepository.save(comment);
-			link.addComment(comment);
-
-			link.addComment(comment);
-
-			System.out.println("We just inserted a link and a comment");
-			System.out.println("=====================================");
-		};
+	@Bean
+	PrettyTime prettyTime() {
+		return new PrettyTime();
 	}
+
 
 }
--- src/main/java/com/chiocchetti/springit/domain/Link.java
+++ src/main/java/com/chiocchetti/springit/domain/Link.java
@@ -1,18 +1,28 @@
 package com.chiocchetti.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import com.chiocchetti.springit.service.BeanUtil;
+import lombok.*;
+import org.hibernate.validator.constraints.URL;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
-@Data
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 public class Link extends Auditable {
 
@@ -20,21 +30,35 @@ public class Link extends Auditable {
     @GeneratedValue
     private Long id;
     @NonNull
+    @NotEmpty(message = "Please enter a title")
     private String title;
     @NonNull
+    @NotEmpty(message = "Please enter a URL")
+    @URL(message = "Please enter a valid URL")
     private String url;
 
     // comments
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
 
-    public Link(String title, String url) {
-        this.title = title;
-        this.url = url;
+    public void addComment(Comment comment) {
+
+        comments.add(comment);
     }
 
-    public void addComment(Comment comment) {
-        comments.add(comment);
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 
 }

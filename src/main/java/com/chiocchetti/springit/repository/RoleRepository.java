9458becee52ac060f417c6092196475a0ee04ae8
Noel Chiocchetti <nc@chiocchetti.ch>
2019-04-12 09:06:05 +0200

    H2 Console Security configuration

---
+++ src/main/java/com/chiocchetti/springit/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.chiocchetti.springit.repository;
+
+import com.chiocchetti.springit.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
---
+++ src/main/java/com/chiocchetti/springit/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.chiocchetti.springit.repository;
+
+import com.chiocchetti.springit.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
